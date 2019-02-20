bb2c48a6f55071d4b9fed5de0ae9a5163e10f62b
sara0alyahya <sara.a.alyahya@outlook.com>
2019-02-20 16:05:01 +0300

    Updates

---
+++ precache-manifest.a9989bdab1228885b27b629762084255.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b82a547fb3e3e210e2b9",
+    "url": "/todo-list/static/css/main.b30c96c5.chunk.css"
+  },
+  {
+    "revision": "b82a547fb3e3e210e2b9",
+    "url": "/todo-list/static/js/main.b82a547f.chunk.js"
+  },
+  {
+    "revision": "86494e9c6d32fb67be2b",
+    "url": "/todo-list/static/js/runtime~main.86494e9c.js"
+  },
+  {
+    "revision": "6513a1bc041d33539bd8",
+    "url": "/todo-list/static/css/2.09e170c2.chunk.css"
+  },
+  {
+    "revision": "6513a1bc041d33539bd8",
+    "url": "/todo-list/static/js/2.6513a1bc.chunk.js"
+  },
+  {
+    "revision": "7a817979a299febe18779e4bb3b4c0a2",
+    "url": "/todo-list/index.html"
+  }
+];
